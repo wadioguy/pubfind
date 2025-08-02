37428e29c3aa8ec2f73fa04553e9ed0be341d9fd
Wade Bush <wade@wadesinnovations.com>
2025-08-02 14:43:28 +0800

    fix: add custom leaflet type declaration

--- types/leaflet.d.ts
+++ types/leaflet.d.ts
@@ -1,1 +1,7 @@
-declare module 'leaflet';
+// types/leaflet.d.ts
+
+declare module 'leaflet' {
+  // Use this temporary fallback only if @types/leaflet doesn't work
+  const L: any;
+  export default L;
+}
