89151e783af8b821bcdaa1f75ab628f90d897f4c
Wade Bush <wade@wadesinnovations.com>
2025-08-02 14:14:10 +0800

    chore: remove leaflet type stub

--- types/leaflet.d.ts
+++
@@ -1,8 +0,0 @@
-// types/leaflet.d.ts
-// Minimal stub to silence “cannot find declaration file” errors
-declare module 'leaflet' {
-  const L: any;            // you can replace `any` with real interfaces later
-  export = L;
-}
-
-declare module 'react-leaflet';
